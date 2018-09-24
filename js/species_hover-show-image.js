db89c738bdc9a4a141664effb026a4ddc551b6d8
Alden Dirks <aldendirks@gmail.com>
2018-09-24 16:17:09 -0500

    site overhaul

---
+++ js/species_hover-show-image.js
@@ -0,0 +1,23 @@
+// Species page script
+
+var speciesImage = document.getElementById('species-page-image-id');
+
+var galziniaIncrustans = document.getElementById('galziniaIncrustans');
+galziniaIncrustans.onmouseover = function() {
+	speciesImage.src = "";
+};
+
+var lindtneriaLeucobryophila = document.getElementById('lindtneriaLeucobryophila');
+galziniaIncrustans.onmouseover = function() {
+	speciesImage.src = "/images/Species/Galzinia incrustans/BHI-F664_7_small.jpg";
+};
+
+var sistotremastrumGuttuliferum = document.getElementById('sistotremastrumGuttuliferum');
+sistotremastrumGuttuliferum.onmouseover = function() {
+	speciesImage.src = "/images/Species/Sistotremastrum guttuliferum/BHI-F756_modified_small.jpg";
+};
+
+var subulicystidiumSp = document.getElementById('subulicystidiumBrachysporum');
+subulicystidiumSp.onmouseover = function() {
+	speciesImage.src = "/images/Species/Subulicystidium brachysporum/IMG_5048_small.jpg";
+};
